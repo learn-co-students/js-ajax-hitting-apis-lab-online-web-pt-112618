81919c65cefee923ac99704bb7fff31cc53a80c1
Chris Philbin <chris@chrisphilbin.net>
2019-07-06 10:15:07 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// your code here
+const root = 'https://api.github.com'
+
+function getRepositories() {
+	let name = document.getElementById("username").value
+	let uri = root + '/users/' + name + '/repos'
+	let xhr = new XMLHttpRequest()
+	xhr.addEventListener('load', displayRepositories)
+	xhr.open('GET', uri)
+	xhr.send()
+	return false
+}
